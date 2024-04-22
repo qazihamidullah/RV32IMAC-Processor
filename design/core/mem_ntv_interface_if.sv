faa10176e53a47756daa2609a9d5ea2468f2d622
Qazi Hamid Ullah <105585212+qazihamidullah@users.noreply.github.com>
2024-04-22 11:11:22 +0500

    Update mem_ntv_interface_if.sv

--- design/core/mem_ntv_interface_if.sv
+++ design/core/mem_ntv_interface_if.sv
@@ -1,7 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
-/// Author (name and email): Afif Arif Siddiqi, asiddiqi.bee20seecs@seecs.edu.pk
-/// Date Created: 1/24/2024
-/// Description: <creating interfaces for DMEM and IMEM>
+/// Author (name and email): Qazi Hamid Ullah, qullah.bee20seecs@seecs.edu.pk
+/// Date Created: 2/24/2024
 ///////////////////////////////////////////////////////////////////////////
 
 
