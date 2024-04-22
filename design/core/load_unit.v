424164150cced6db6c8c05caaf6c61a335c4c373
Qazi Hamid Ullah <105585212+qazihamidullah@users.noreply.github.com>
2024-04-22 11:11:10 +0500

    Update load_unit.v

--- design/core/load_unit.v
+++ design/core/load_unit.v
@@ -1,7 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
-/// Author (name and email): Afif Arif Siddiqi, asiddiqi.bee20seecs@seecs.edu.pk
-/// Date Created: 1/24/2024
-/// Description: <load data in register according to load instructions>
+/// Author (name and email): Qazi Hamid Ullah, qullah.bee20seecs@seecs.edu.pk
+/// Date Created: 2/24/2024
 ///////////////////////////////////////////////////////////////////////////
 
 module load_unit
